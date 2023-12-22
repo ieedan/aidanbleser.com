595e71bfaecb909416a5c30d68e6c93ec3f10e3e
Aidan Bleser <aidanbleser35@gmail.com>
2023-12-22 10:46:51 -0600

    added for debug

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,7 +1,9 @@
 import { posts } from "$lib/blog/posts/posts";
 
 export function load({ params }) {
+	const index = posts.findIndex((a) => a.title == params.slug);
 	return {
-		index: posts.findIndex((a) => a.title == params.slug),
+		index: index,
+		title: posts[index].title 
 	};
 }
