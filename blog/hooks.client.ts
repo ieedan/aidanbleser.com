2e9d087be272663218ccf6d3237e6af7c3fb7e03
Aidan Bleser <aidanbleser35@gmail.com>
2024-05-07 18:51:33 -0500

    added speed insights to blog as well

---
+++ blog/hooks.client.ts
@@ -0,0 +1,11 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+inject({ mode: dev ? 'development' : 'production' });
+
+// eslint-disable-next-line no-console
+console.log(
+	'Welcome to blog.aidanbleser.com! See the source here https://github.com/ieedan/aidanbleser.com! 🚀'
+);
