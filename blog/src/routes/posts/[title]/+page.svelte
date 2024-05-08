ce33c2d5ca07446b0f2a97bbe33189b8a382a65d
Aidan Bleser <aidanbleser35@gmail.com>
2024-05-08 10:48:47 -0500

    temp fix for invalid date

--- blog/src/routes/posts/[title]/+page.svelte
+++ blog/src/routes/posts/[title]/+page.svelte
@@ -10,9 +10,9 @@
 
 <main class="flex flex-col place-items-center px-4">
 	<div class="flex w-full max-w-[65ch] place-items-center justify-between">
-		<small class="text-foreground-secondary"
-			>{new Date(post.metaData.date).toLocaleDateString()}</small
-		>
+		<small class="text-foreground-secondary">
+			{post.metaData.date}
+		</small>
 		<span class="text-xs text-foreground-secondary">{post.metaData.timeToRead} min read</span>
 	</div>
 	<div class="prose prose-zinc py-4 dark:prose-invert prose-code:whitespace-pre-wrap">
