8b23392f9e6575cf7ce75974899f7539f55c4c0b
Aidan Bleser <aidanbleser35@gmail.com>
2026-02-03 17:07:02 -0600

    more

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -9,17 +9,33 @@
 
 <div class="relative flex w-full max-w-4xl border border-border">
 	<div class="flex flex-1 flex-col">
-		<div class="flex-1 p-4">
-			<div class="flex items-center gap-4">
+		<div class="flex flex-1 flex-col">
+			<div class="flex w-full items-center gap-4 border-b p-4">
 				<img
 					src="https://avatars.githubusercontent.com/ieedan"
 					alt="Aidan Bleser"
-					class="size-14 shrink-0 rounded-full"
+					class="size-12 shrink-0 rounded-full"
 				/>
 				<div class="flex flex-col">
 					<h1 class="text-2xl">Aidan Bleser</h1>
 					<span class="text-sm text-muted-foreground">Frontend Engineer</span>
 				</div>
+			</div>
+			<div class="flex flex-col gap-4 p-4">
+				<p>
+					I'm a software engineer from San Antonio, Texas. I learned to code to automate my KPIs as
+					a maintenance supervisor and I ended up building (and rebuilding, and rebuilding again)
+					the CMMS (Computerized Maintenance Management System) now used by Avanzar Interior
+					Technologies.
+				</p>
+				<p>
+					I started contributing to open source in 2024 and since have become a maintainer of
+					several popular projects such as, shadcn-svelte, jsrepo, and shadcn-svelte-extras.
+				</p>
+				<p>
+					If I'm not slaving (or rather watching Cursor slave) behind my desk I'm probably cycling,
+					skiing, hiking, or just spending time with my beautiful wife.
+				</p>
 			</div>
 		</div>
 		<footer class="border-t border-border p-4">
